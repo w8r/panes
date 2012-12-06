7e2a82f736ac48ef90f6b48fa944075d1bcb071b
w8r <alexander.milewski@gmail.com>
2012-12-06 12:17:02 +0300

    cleaned up, JsDoc

--- src/js/pane.js
+++ src/js/pane.js
@@ -1,17 +1,20 @@
-(function($, mvc) {
+(function($, View) {
 
     /**
      * Dummy pane view - to inherit from
+     * 
      * @type {View}
      */
     var Pane = this.Pane = mvc.View.extend({
-        initialize: function() {
-            mvc.View.prototype.initialize.apply(this, arguments);
-            this.render();
-        },
-        render: function() {
-            this.el.innerHTML = '<h3>p' + this.model.collection.indexOf(this.model) + '</h3>';
-        }
-    });
+                initialize : function() {
+                    View.prototype.initialize.apply(this, arguments);
+                    this.render();
+                },
+                render : function() {
+                    this.el.innerHTML =
+                            '<h3>p' + this.model.collection.indexOf(this.model)
+                                    + '</h3>';
+                }
+            });
 
-})(jQuery, Backbone);
+})(jQuery, Backbone.View);
--- src/js/panes.js
+++ src/js/panes.js
@@ -1,53 +1,89 @@
-(function($, mvc) {
+(function($, Model, View, Collection) {
 
     var getStyle = function(el, styleProp) {
         var value = el.style[styleProp];
-        if(value === '') {
-            if(el.currentStyle) {
+        if (value === '') {
+            if (el.currentStyle) {
                 value = el.currentStyle[styleProp];
             } else {
-                if(window.getComputedStyle) {
-                    value = document.defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
+                if (window.getComputedStyle) {
+                    value =
+                            document.defaultView.getComputedStyle(el, null)
+                                    .getPropertyValue(styleProp);
                 }
             }
         }
         return value;
     },
 
-        isArray = typeof Array.isArray === 'function' ? Array.isArray : function(object) {
-        return Object.prototype.toString.call(object) == '[object Array]';
+        isArray =
+                typeof Array.isArray === 'function' ? Array.isArray : function(
+                        object) {
+                    return Object.prototype.toString.call(object) == '[object Array]';
+                };
+
+    // https://gist.github.com/1312328
+    Function.prototype.bind = Function.prototype.bind || function(b) {
+        if (typeof this !== "function") {
+            throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+        }
+        var a = Array.prototype.slice,
+            f = a.call(arguments, 1),
+            e = this,
+            c = function() {},
+            d = function() {
+                return e.apply(this instanceof c ? this : b || window, f
+                                .concat(a.call(arguments)));
+            };
+        c.prototype = this.prototype;
+        d.prototype = new c();
+        return d;
     };
 
-    var Panes = this.Panes = mvc.View.extend({
-        tagName: 'div',
-        className: 'panes',
-        defaultView: Pane,
-        bufferPanes: 3,
-        firstVisiblePane: 0,
-        viewportSize: null,
+    /**
+     * @class Panes
+     */
+    var Panes = this.Panes = View.extend({
+        tagName : 'div',
+        className : 'panes',
+        defaultView : Pane,
+        bufferPanes : 3,
+        firstVisiblePane : 0,
+        viewportSize : null,
 
-        initialize: function(options) {
+        /**
+         * @constructor
+         * @param {Object}
+         *            options
+         */
+        initialize : function(options) {
             console.log('Panes:constructor', options);
             _.bindAll(this, 'addPane', 'removePane', 'insertPane');
 
             this.model.on('add', this.addPane);
             this.model.on('remove', this.removePane);
 
-            mvc.View.prototype.initialize.apply(this, arguments);
+            View.prototype.initialize.apply(this, arguments);
 
             this.updateViewportSize();
             this.createCanvas();
             this.paneWidth = this.measurePane();
-            this.panesPerViewport = Math.floor(this.viewportSize.w / this.paneWidth);
+            this.panesPerViewport =
+                    Math.floor(this.viewportSize.w / this.paneWidth);
             this.adjustCanvasToViewport();
 
             this.render();
         },
 
-        updateViewportSize: function() {
+        /**
+         * Stores viewport size
+         * 
+         * @returns {Object} size
+         */
+        updateViewportSize : function() {
             var size = this.viewportSize = {
-                w: this.el.offsetWidth,
-                h: this.el.offsetHeight
+                w : this.el.offsetWidth,
+                h : this.el.offsetHeight
             };
             return size;
         },
@@ -55,15 +91,16 @@
         /**
          * @return {Number}
          */
-        getViewportWidth: function() {
+        getViewportWidth : function() {
             return this.el.clientWidth;
         },
 
         /**
          * Creates movable container for panes
+         * 
          * @return {HTMLElement}
          */
-        createCanvas: function() {
+        createCanvas : function() {
             var container = document.createElement('div');
             container.className = 'panes-container';
             this.el.appendChild(container);
@@ -74,8 +111,9 @@
         /**
          * Adjust canvas to contain buffer space to the left
          */
-        adjustCanvasToViewport: function() {
-            var bufferMargin = this.bufferMargin = (this.bufferPanes + 1) * this.paneWidth,
+        adjustCanvasToViewport : function() {
+            var bufferMargin =
+                    this.bufferMargin = (this.bufferPanes + 1) * this.paneWidth,
                 width = this.viewportSize.w + bufferMargin;
             this.container.style.width = width + 'px';
             this.container.style.height = this.viewportSize.h + 'px';
@@ -84,10 +122,12 @@
 
         /**
          * Measures particular pane or dummy pane if none passed
-         * @param  {HTMLElement} pane
+         * 
+         * @param {HTMLElement}
+         *            pane
          * @return {Number} pane width including margins
          */
-        measurePane: function(pane) {
+        measurePane : function(pane) {
             // console.time('measurePane');
             pane = pane || this.createPane();
             var width, margins;
@@ -96,7 +136,9 @@
             this.container.appendChild(pane);
 
             margins = getStyle(pane, 'margin').split(' ');
-            width = parseInt(pane.clientWidth) + parseInt(margins[1]) + parseInt(margins[3]);
+            width =
+                    parseInt(pane.clientWidth) + parseInt(margins[1])
+                            + parseInt(margins[3]);
 
             this.container.removeChild(pane);
             // console.timeEnd('measurePane');
@@ -105,37 +147,51 @@
 
         /**
          * Adds a pane at random position, at the top of the stack by default
-         * @param {Model} model
-         * @param {Collection} collection
-         * @param {Object} [options]
-         * @param {Number} [options.at] position
-         * @param {Number} [options.index] model index
+         * 
+         * @param {Model}
+         *            model
+         * @param {Collection}
+         *            collection
+         * @param {Object}
+         *            [options]
+         * @param {Number}
+         *            [options.at] position
+         * @param {Number}
+         *            [options.index] model index
+         * @param {HTMLElement|DocumentFragment}
+         *            options.container Container to insert pane into
          */
-        addPane: function(model, collection, options) {
+        addPane : function(model, collection, options) {
             console.group('addPane');
             console.log('Panes:addPane', arguments, options.index, options.at);
             options = options || {};
-            var pos = options.index || 0,
-                pane = this.createPane(model);
+            var pos = options.index || collection.indexOf(model),
+                pane = this.createPane(model),
+                container = options.container || this.container;
 
             // first pane - position
-            if(pos === 0) {
+            if (pos === 0) {
                 pane.style.marginLeft = this.bufferMargin + 'px';
-            } else if(this.model.length > this.panesPerViewport) {
+            } else if (collection.length > this.panesPerViewport) {
                 // if there is a margin buffer to use - use it
-                if(this.bufferMargin > this.paneWidth) {
+                if (this.bufferMargin > this.paneWidth) {
                     this.bufferMargin -= this.paneWidth;
-                    this.model.models[0]._view.el.style.marginLeft = this.bufferMargin + 'px';
+                    collection.models[0]._view.el.style.marginLeft =
+                            this.bufferMargin + 'px';
                 } else {
                     // else start removing(collapsing) panes
                     // that are out of bounds
-                    var first = this.model.models[this.firstVisiblePane]._view.el,
-                        next = this.model.models[this.firstVisiblePane + 1]._view.el;
+                    var firstVisible = this.firstVisiblePane,
+                        first = collection.models[firstVisible]._view.el,
+                        next = collection.models[firstVisible + 1]._view.el;
 
+                    // copy margin to the next one
                     next.style.marginLeft = first.style.marginLeft;
                     first.style.marginLeft = '';
+                    // constrict
                     first.style.display = 'none';
 
+                    // set iterator to the first expanded
                     this.firstVisiblePane++;
                 }
             }
@@ -145,30 +201,45 @@
             console.groupEnd('addPane');
         },
 
-        preparePlace: function(model) {},
-
-        removePane: function(model, collection, options) {
+        /**
+         * Removes random pane
+         * 
+         * @param {Model}
+         *            model Pane model
+         * @param {Collection}
+         *            collection
+         * @param {Object}
+         *            options
+         * @param {Number}
+         *            options.index Insertion index
+         */
+        removePane : function(model, collection, options) {
             console.group('removePane');
-            console.log('Panes:removePane', arguments, this.model.length, this.panesPerViewport);
+            console.log('Panes:removePane', arguments, this.model.length,
+                    this.panesPerViewport);
             options = options || {};
             var pos = collection.indexOf(model),
                 pane = model._view.el;
 
-            if(this.model.length >= this.panesPerViewport) {
+            if (collection.length >= this.panesPerViewport) {
                 // there are panes out of view, move them forward
-                var first, prev, margin;
-                if(this.firstVisiblePane === 0) {
-                    first = this.model.models[0]._view.el;
+                var firstVisible = this.firstVisiblePane, first, prev;
+                if (firstVisible === 0) {
+                    // no constricted panes - move first one to the right
+                    first = collection.models[0]._view.el;
                     this.bufferMargin += this.paneWidth;
                     first.style.marginLeft = this.bufferMargin + 'px';
                 } else {
-                    first = this.model.models[this.firstVisiblePane]._view.el;
-                    prev = this.model.models[this.firstVisiblePane - 1]._view.el;
+                    first = collection.models[firstVisible]._view.el;
+                    prev = collection.models[firstVisible - 1]._view.el;
 
+                    // copy margin from the expanded one
                     prev.style.marginLeft = first.style.marginLeft;
                     first.style.marginLeft = '';
+                    // then expand
                     prev.style.display = 'block';
 
+                    // it is now first expanded pane
                     this.firstVisiblePane--;
                 }
             }
@@ -176,78 +247,71 @@
             console.groupEnd('removePane');
         },
 
-        insertPane: function() {
+        insertPane : function() {
             console.log('Panes:insertPane', arguments);
         },
 
         /**
-         * Creates panes for collection
-         * @param  {Array/Model} models
-         * @return {documentFragment} containing all the created panes, positioned
+         * Adds pane to the model view as the container(.el)
+         * 
+         * @param {Model}
+         *            model
+         * @param {HTMLElement}
+         *            pane
+         * @returns {HTMLElement} pane
          */
-        createPanes: function(models) {
-            console.log('Panes#createPanes', arguments);
-            if(!isArray(models)) {
-                models = [models];
-            }
-            var fragment = document.createDocumentFragment(),
-                pane, width = 0,
-                model;
-
-            for(var i = 0, len = models.length; i < len; i++) {
-                model = models[i];
-
-                pane = this.createPane(model);
-                pane.style.left = this.getPositionForPane(i) + 'px';
-
-                this.addView(model, pane);
-
-                fragment.appendChild(pane);
-            }
-            return fragment;
-        },
-
-        getPanePosition: function(model) {
-            var pane = model._view.el;
-            return parseInt(getStyle(pane, 'left'));
-        },
-
-        getPositionForPane: function(index) {
-            var left = 0;
-            if(index > 0) {
-                // get previous pane position, otherwise it won't work
-                left = this.getPanePosition(this.model.models[index - 1]) + this.paneWidth;
-            }
-            return left;
-        },
-
-        addView: function(model, pane) {
+        addView : function(model, pane) {
             var viewOptions = {
-                model: model,
-                el: pane
+                model : model,
+                el : pane
             },
                 viewConstructor = model.view || this.defaultView;
             model._view = new viewConstructor(viewOptions);
-            return model;
+            return pane;
         },
 
-        removeView: function(model, pane) {
+        /**
+         * Removes pane reference from model view, destroys element
+         * 
+         * @param {Model}
+         *            model
+         * @param {HTMLElement}
+         *            pane
+         * @returns {HTMLElement} detached pane
+         */
+        removeView : function(model, pane) {
             pane.parentNode.removeChild(pane);
             delete model._view;
+            return pane;
         },
 
-        createPane: function() {
+        /**
+         * Creates pane element
+         * 
+         * @returns {HTMLElement}
+         */
+        createPane : function() {
             var pane = document.createElement('div');
             pane.className = 'pane';
             return pane;
         },
 
-        render: function() {
+        /**
+         * First render
+         */
+        render : function() {
             console.log('Panes:render', arguments);
-            // create all the panes at once
-            // this.container.appendChild(this.createPanes(this.model.models));
+            // create all the panes at once, use DocumentFragment to boost
+            var paneOptions = {
+                container : document.createDocumentFragment()
+            };
+            for (var i = 0, len = this.model.length; i < len; i++) {
+                paneOptions.index = i;
+                this.addPane(this.model.models[i], this.model, paneOptions);
+            }
+            this.container.appendChild(paneOptions.container);
         }
 
     });
 
-})(jQuery, Backbone);
+})(jQuery, Backbone.Model, Backbone.View, Backbone.Collection);
